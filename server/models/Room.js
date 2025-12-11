81b5706cc31838fd177ecdc5b53d4aaf48ab49e3
jahid611 <jahidsayad42@gmail.com>
2025-12-12 00:36:55 +0100

    go

--- server/models/Room.js
+++ server/models/Room.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
 const RoomSchema = new mongoose.Schema({
-    slug: { type: String, required: true, unique: true },
-    ownerEmail: { type: String }, // <--- NOUVEAU : Le chef du débat
-    game: { type: String, default: "autre" },
-    videoUrl: { type: String, default: "" },
-    videoTitle: { type: String, default: "" },
-    videoPlatform: { type: String, default: "" },
-    votes: {
-        valid: [{ type: String }],
-        inting: [{ type: String }]
-    },
-    updatedAt: { type: Date, default: Date.now }
-});
+  slug: { type: String, required: true, unique: true },
+  ownerEmail: { type: String },
+  game: { type: String, default: "autre" },
+  videoUrl: { type: String, default: "" },
+  videoTitle: { type: String, default: "Nouveau Debat" },
+  videoDescription: { type: String, default: "" },
+  videoPlatform: { type: String, default: "" },
+  votes: {
+    valid: [{ type: String }],
+    inting: [{ type: String }],
+  },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+})
 
-module.exports = mongoose.model('Room', RoomSchema);
+module.exports = mongoose.model("Room", RoomSchema)
