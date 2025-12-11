5602bb77ad2b819eaf3dcb36e578d5d2d6c6bdf7
jahid611 <jahidsayad42@gmail.com>
2025-12-11 02:15:10 +0100

    email doulon fix

--- server/models/User.js
+++ server/models/User.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    // Google ID n'est plus obligatoire (required: false)
-    googleId: { type: String, required: false },
+    googleId: { 
+        type: String, 
+        required: false, 
+        unique: true, 
+        sparse: true // <--- C'EST LA CLÉ MAGIQUE ! (Ignore les nulls)
+    },
     
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: false }, // Nouveau champ pour le mode manuel
+    password: { type: String, required: false },
     
     pseudo: { type: String },
     name: { type: String },
     age: { type: Number },
-    avatar: { type: String, default: "https://cdn-icons-png.flaticon.com/512/847/847969.png" }, // Avatar par défaut
+    avatar: { type: String, default: "https://cdn-icons-png.flaticon.com/512/847/847969.png" },
     
     games: [String],
     details: { type: Object },
