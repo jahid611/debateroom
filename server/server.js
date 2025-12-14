757220801f1fea929f745b494887992ac9b2bdf6
jahid611 <jahidsayad42@gmail.com>
2025-12-14 04:17:26 +0100

    cssc

--- server/server.js
+++ server/server.js
@@ -14,6 +14,12 @@ const server = http.createServer(app)
 
 app.use(cors())
 app.use(express.json())
+app.use((req, res, next) => {
+    // Autorise les popups (Google Login) et les iframes (YouTube) à communiquer
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+    res.setHeader("Cross-Origin-Embedder-Policy", "unsafe-none"); 
+    next();
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
